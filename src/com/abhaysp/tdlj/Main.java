be3c4d421215a78520fe145f43106a9d2ccba606
coolabhays <abhaysp9955@gmail.com>
2020-12-12 23:30:24 +0530

    override init() and stop() method

--- src/com/abhaysp/tdlj/Main.java
+++ src/com/abhaysp/tdlj/Main.java
@@ -1,5 +1,9 @@
 package com.abhaysp.tdlj;
 
+import java.io.IOException;
+
+import com.abhaysp.tdlj.datamodel.TodoData;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -9,12 +13,34 @@ import javafx.fxml.FXMLLoader;
 public class Main extends Application {
 
 	@Override
+	public void init() throws Exception {
+		try {
+			TodoData.getInstance().loadTodoItems();
+		}
+		catch (IOException ie) {
+			System.out.println(ie.getMessage());
+		}
+	}
+
+	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/com/abhaysp/tdlj/mainwindow.fxml"));
 		primaryStage.setTitle("tdlj");
 		primaryStage.setScene(new Scene(root, 900, 400));
 		primaryStage.show();
 	}
+
+	@Override
+	public void stop() throws Exception {
+		try {
+			// use Singleton TodoData
+			TodoData.getInstance().storeTodoItems();
+		}
+		catch (IOException ie) {
+			System.out.println(ie.getMessage());
+		}
+	}
+
 	public static void main(String ...args) {
 		launch(args);
 	}
